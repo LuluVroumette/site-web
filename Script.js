ef07457d3bbe0e2e2c03a4a52d707cad26839ec3
Etric3001 <tathanric304@gmail.com>
2024-10-27 18:12:34 +0100

    images de types

--- Script.js
+++ Script.js
@@ -7,15 +7,24 @@ const apiUrl = 'https://tyradex.vercel.app/api/v1/gen/1';
         }
 
         // Infos que je veux sur page
-        function showPokemonDetails(pokemon) {
+        function informations(pokemon) {
             document.getElementById('pokemon-name').textContent = pokemon.name.fr;
             document.getElementById('pokemon-image').src = pokemon.sprites.regular;
             document.getElementById('pokemon-image-shiny').src = pokemon.sprites.shiny;
             document.getElementById('pokemon-types').textContent = `Types: ${pokemon.types.map(type => type.name).join(', ')}`;
-            document.getElementById('pokemon-type1').src = pokemon.types.image;
             document.getElementById('pokemon-stats').textContent = `HP: ${pokemon.stats.hp}, Attaque: ${pokemon.stats.atk}, Défense: ${pokemon.stats.def}, Attaque spéciale ${pokemon.stats.spe_atk}, Défense spéciale ${pokemon.stats.spe_def}, Vitesse ${pokemon.stats.vit}`;
-        }
-
+            
+ 
+            const typesConteneur = document.getElementById('pokemon_types_images');
+            typesConteneur.innerHTML = ''; // Pour reset pour pas mettre trente imgs
+            // Parcour chaque type  de api etmet image par typs
+            pokemon.types.forEach(type => {
+                const img = document.createElement('img');
+                img.src = type.image; 
+                img.alt = `Type ${type.name}`; 
+                typesConteneur.appendChild(img); // Ajoute image à div
+            });}
+        
         // def variable avec ID
         const pokemonId = getPokemonIdFromURL();
 
@@ -30,7 +39,7 @@ const apiUrl = 'https://tyradex.vercel.app/api/v1/gen/1';
 
                     // si ça marche prend fonction et montre ce qui est mis dedans
                     if (pokemon) {
-                        showPokemonDetails(pokemon);}})
+                        informations(pokemon);}})
 
                 .catch(error => console.error('Erreur de fetch :', error));}
 
